76b573f7487508138ac463fa7399963e9379dd5c
poramate-to <poramate.to@tnis.com>
2023-01-13 11:32:09 +0700

    add farewell controller test

---
+++ complete/src/test/java/com/example/restservice/FarewellControllerTests.java
@@ -0,0 +1,36 @@
+
+package com.example.restservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes=MainApplication.class)
+public class FarewellControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void bye_shouldReturnByeBye() throws Exception {
+
+		MvcResult result = this.mockMvc.perform(get("/bye"))//.andDo(print())
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String content = result.getResponse().getContentAsString();
+
+		assertEquals("<H1>Bye Bye</H1>", content);
+
+
+	}
+
+}
--- complete/src/test/java/com/example/restservice/GreetingControllerTests.java
+++ complete/src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *	  https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.restservice;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -36,21 +21,6 @@ public class GreetingControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 
-
-
-	@Test
-	public void bye_shouldReturnByeBye() throws Exception {
-
-		MvcResult result = this.mockMvc.perform(get("/bye"))//.andDo(print())
-				.andExpect(status().isOk())
-				.andReturn();
-
-		String content = result.getResponse().getContentAsString();
-
-		assertEquals("<H1>Bye Bye</H1>", content);
-
-
-	}
 
 	@Test
 	public void hiWithNickname_shouldReturnJSONWithNicknameField() throws Exception {
